021fda06b2a955d23ede5f0f26f9a668957d5b0b
louvick <louvick78@yahoo.com>
2023-11-10 13:35:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// TEST2
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use("/api/converter", require("./routes/api/converter"));
+
+app.get("/", (req, res) => {
+    res.send("Home sweet home");
+});
+
+app.listen(80, () => console.log('Server started'));
